02920215cb851c82e2b9b8c9603ab2c7843cc4ed
barnabeepickle <picklebarnabee@gmail.com>
2025-12-01 14:08:46 -0500

    fixed most of BabyRocketSquidModel.java though I still need to fix setRotationPoint

--- src/main/java/com/fredtargaryen/rocketsquids/client/model/BabyRocketSquidModel.java
+++ src/main/java/com/fredtargaryen/rocketsquids/client/model/BabyRocketSquidModel.java
@@ -1,71 +1,71 @@
 package com.fredtargaryen.rocketsquids.client.model;
 
 import com.fredtargaryen.rocketsquids.entity.BabyRocketSquidEntity;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.model.ModelRenderer;
-import net.minecraft.client.renderer.model.Model;
-import net.minecraft.entity.Entity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.Model;
+import net.minecraft.world.entity.Entity;
 
 /**
  * BabyRocketSquidModel - FredTargaryen
  * Created using Tabula 7.0.0
  */
-public class BabyRocketSquidModel extends EntityModel<BabyRocketSquidEntity> {
-    public ModelRenderer Head;
-    public ModelRenderer Tent1;
-    public ModelRenderer Tent2;
-    public ModelRenderer Tent3;
-    public ModelRenderer Tent4;
-    public ModelRenderer Tent5;
-    public ModelRenderer Tent6;
-    public ModelRenderer Tent7;
-    public ModelRenderer Tent8;
+public abstract class BabyRocketSquidModel extends EntityModel<BabyRocketSquidEntity> {
+    public ModelPart Head;
+    public ModelPart Tent1;
+    public ModelPart Tent2;
+    public ModelPart Tent3;
+    public ModelPart Tent4;
+    public ModelPart Tent5;
+    public ModelPart Tent6;
+    public ModelPart Tent7;
+    public ModelPart Tent8;
 
     public BabyRocketSquidModel() {
-        this.textureWidth = 32;
-        this.textureHeight = 32;
-        this.Head = new ModelRenderer(this, 0, 6);
+        this.texWidth = 32;
+        this.texHeight = 32;
+        this.Head = new ModelPart(this, 0, 6);
         this.Head.mirror = true;
         this.Head.setRotationPoint(0.0F, 0.0F, 0.0F);
         this.Head.addBox(-2.0F, -3.0F, -2.0F, 5, 7, 5, 0.0F);
 
-        this.Tent1 = new ModelRenderer(this, 0, 0);
+        this.Tent1 = new ModelPart(this, 0, 0);
         this.Tent1.setRotationPoint(-0.5F, 4.0F, -0.5F);
         this.Tent1.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent1, 0.0F, -2.356194490192345F, 0.0F);
 
-        this.Tent2 = new ModelRenderer(this, 0, 0);
+        this.Tent2 = new ModelPart(this, 0, 0);
         this.Tent2.setRotationPoint(0.5F, 4.0F, -1.0F);
         this.Tent2.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent2, 0.0F, 3.141592653589793F, 0.0F);
 
-        this.Tent3 = new ModelRenderer(this, 0, 0);
+        this.Tent3 = new ModelPart(this, 0, 0);
         this.Tent3.setRotationPoint(1.5F, 4.0F, -0.5F);
         this.Tent3.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent3, 0.0F, 2.356194490192345F, 0.0F);
 
-        this.Tent4 = new ModelRenderer(this, 0, 0);
+        this.Tent4 = new ModelPart(this, 0, 0);
         this.Tent4.setRotationPoint(2.0F, 4.0F, 0.5F);
         this.Tent4.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent4, 0.0F, 1.5707963267948966F, 0.0F);
 
-        this.Tent5 = new ModelRenderer(this, 0, 0);
+        this.Tent5 = new ModelPart(this, 0, 0);
         this.Tent5.setRotationPoint(1.5F, 4.0F, 1.5F);
         this.Tent5.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent5, 0.0F, 0.7853981633974483F, 0.0F);
 
-        this.Tent6 = new ModelRenderer(this, 0, 0);
+        this.Tent6 = new ModelPart(this, 0, 0);
         this.Tent6.setRotationPoint(0.5F, 4.0F, 2.0F);
         this.Tent6.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
 
-        this.Tent7 = new ModelRenderer(this, 0, 0);
+        this.Tent7 = new ModelPart(this, 0, 0);
         this.Tent7.setRotationPoint(-0.5F, 4.0F, 1.5F);
         this.Tent7.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent7, 0.0F, -0.7853981633974483F, 0.0F);
 
-        this.Tent8 = new ModelRenderer(this, 0, 0);
+        this.Tent8 = new ModelPart(this, 0, 0);
         this.Tent8.setRotationPoint(-1.0F, 4.0F, 0.5F);
         this.Tent8.addBox(-0.5F, 0.0F, -0.5F, 1, 5, 1, 0.0F);
         this.setRotateAngle(Tent8, 0.0F, -1.5707963267948966F, 0.0F);
@@ -86,7 +86,7 @@ public class BabyRocketSquidModel extends EntityModel<BabyRocketSquidEntity> {
 //        this.Tent8.render(f5);
 //    }
 
-    public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float r, float g, float b, float a) {
+    public void render(PoseStack matrixStackIn, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float r, float g, float b, float a) {
         this.Head.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, r, g, b, a);
         this.Tent1.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, r, g, b, a);
         this.Tent2.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, r, g, b, a);
@@ -101,21 +101,21 @@ public class BabyRocketSquidModel extends EntityModel<BabyRocketSquidEntity> {
     /**
      * This is a helper function from Tabula to set the rotation of model parts
      */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-        modelRenderer.rotateAngleX = x;
-        modelRenderer.rotateAngleY = y;
-        modelRenderer.rotateAngleZ = z;
+    public void setRotateAngle(ModelPart ModelPart, float x, float y, float z) {
+        ModelPart.x = x;
+        ModelPart.y = y;
+        ModelPart.z = z;
     }
 
     @Override
-    public void setRotationAngles(BabyRocketSquidEntity entity, float time, float maxSpeed, float whatever, float rotationYaw, float rotationPitch) {
-        this.Tent1.rotateAngleX = whatever;
-        this.Tent2.rotateAngleX = whatever;
-        this.Tent3.rotateAngleX = whatever;
-        this.Tent4.rotateAngleX = whatever;
-        this.Tent5.rotateAngleX = whatever;
-        this.Tent6.rotateAngleX = whatever;
-        this.Tent7.rotateAngleX = whatever;
-        this.Tent8.rotateAngleX = whatever;
+    public void setupAnim(BabyRocketSquidEntity entity, float time, float maxSpeed, float whatever, float rotationYaw, float rotationPitch) {
+        this.Tent1.x = whatever;
+        this.Tent2.x = whatever;
+        this.Tent3.x = whatever;
+        this.Tent4.x = whatever;
+        this.Tent5.x = whatever;
+        this.Tent6.x = whatever;
+        this.Tent7.x = whatever;
+        this.Tent8.x = whatever;
     }
 }
