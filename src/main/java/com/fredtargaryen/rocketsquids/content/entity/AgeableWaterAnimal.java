7f1176bddf2a80bd117f984b8e92aef663b9cf8c
barnabeepickle <picklebarnabee@gmail.com>
2026-03-13 14:35:38 -0400

    created AgeableWaterAnimal (#32)

---
+++ src/main/java/com/fredtargaryen/rocketsquids/content/entity/AgeableWaterAnimal.java
@@ -0,0 +1,89 @@
+package com.fredtargaryen.rocketsquids.content.entity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class AgeableWaterAnimal extends AgeableMob {
+
+    protected AgeableWaterAnimal(EntityType<? extends AgeableMob> entityType, Level level) {
+        super(entityType, level);
+        this.setPathfindingMalus(BlockPathTypes.WATER, 0.0F);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean canBreatheUnderwater() {
+        return true;
+    }
+
+    @Override
+    public @NotNull MobType getMobType() {
+        return MobType.WATER;
+    }
+
+    @Override
+    public boolean checkSpawnObstruction(LevelReader level) {
+        return level.isUnobstructed(this);
+    }
+
+    @Override
+    public int getAmbientSoundInterval() {
+        return 120;
+    }
+
+    @Override
+    public int getExperienceReward() {
+        return 1 + this.level().random.nextInt(3);
+    }
+
+    protected void handleAirSupply(int airSupply) {
+        if (this.isAlive() && !this.isInWaterOrBubble()) {
+            this.setAirSupply(airSupply - 1);
+            if (this.getAirSupply() == -20) {
+                this.setAirSupply(0);
+                this.hurt(this.damageSources().drown(), 2.0F);
+            }
+        } else {
+            this.setAirSupply(300);
+        }
+    }
+
+    @Override
+    public void baseTick() {
+        int i = this.getAirSupply();
+        super.baseTick();
+        this.handleAirSupply(i);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isPushedByFluid() {
+        return false;
+    }
+
+    @Override
+    public boolean canBeLeashed(@NotNull Player player) {
+        return false;
+    }
+
+    @SuppressWarnings({"deprecation", "unused"})
+    public static boolean checkSurfaceWaterAnimalSpawnRules(
+            EntityType<? extends AgeableWaterAnimal> waterAnimal, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random
+    ) {
+        int i = level.getSeaLevel();
+        int j = i - 13;
+        return pos.getY() >= j && pos.getY() <= i && level.getFluidState(pos.below()).is(FluidTags.WATER) && level.getBlockState(pos.above()).is(Blocks.WATER);
+    }
+}
