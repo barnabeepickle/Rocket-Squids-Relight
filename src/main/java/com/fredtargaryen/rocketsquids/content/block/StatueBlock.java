314107f28e639111184b6e940aaa068d40e76bac
fredtargaryen <lggy@hotmail.co.uk>
2026-03-25 22:19:17 +0000

    Implement opening statue from any direction; dispense treasure with a different position and velocity depending on statue rotation

--- src/main/java/com/fredtargaryen/rocketsquids/content/block/StatueBlock.java
+++ src/main/java/com/fredtargaryen/rocketsquids/content/block/StatueBlock.java
@@ -21,10 +21,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
-import java.util.Objects;
 
 import static net.minecraft.world.level.block.HorizontalDirectionalBlock.FACING;
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.DOUBLE_BLOCK_HALF;
@@ -132,23 +132,31 @@ public class StatueBlock extends Block {
         }
     }
 
-    public void dispenseGift(Level world, BlockPos pos, Direction facing) {
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
+    /**
+     * Should only be called by ItemConch
+     */
+    public void dispenseGift(Level level, BlockPos pos, Direction facing) {
         //Play some kind of wonderful "you found treasure" chord
         //Going with B4, D5 and F#5 (the minor chord makes it foreboding)
-        world.playSound(null, pos, ModSounds.CONCH_NOTES[23], SoundSource.BLOCKS, 1.0F, 1.0F);
-        world.playSound(null, pos, ModSounds.CONCH_NOTES[26], SoundSource.BLOCKS, 1.0F, 1.0F);
-        world.playSound(null, pos, ModSounds.CONCH_NOTES[30], SoundSource.BLOCKS, 1.0F, 1.0F);
-        if (Objects.requireNonNull(facing) == Direction.NORTH) {
-            ItemEntity squav = new ItemEntity(world, x + 0.5D, y + 0.5D, z - 0.5D, ModItems.SQUAVIGATOR.get().getDefaultInstance());
-            // North is negative Z I think
-            squav.setDeltaMovement(0.0, 0.05, -0.1);
-            world.addFreshEntity(squav);
-            ItemEntity squel = new ItemEntity(world, x + 0.5D, y + 0.5D, z - 0.5D, ModItems.SQUAVIGATOR.get().getDefaultInstance());
-            squel.setDeltaMovement(0.0, 0.05, -0.1);
-            world.addFreshEntity(squel);
-        }
+        level.playSound(null, pos, ModSounds.CONCH_NOTES[23], SoundSource.BLOCKS, 1.0F, 1.0F);
+        level.playSound(null, pos, ModSounds.CONCH_NOTES[26], SoundSource.BLOCKS, 1.0F, 1.0F);
+        level.playSound(null, pos, ModSounds.CONCH_NOTES[30], SoundSource.BLOCKS, 1.0F, 1.0F);
+        Vec3 treasureVelocity = switch (facing) {
+            case EAST -> new Vec3(0.1, 0.05, 0.0);
+            case NORTH -> new Vec3(0.0, 0.05, -0.1);
+            case SOUTH -> new Vec3(0.0, 0.05, 0.1);
+            default -> new Vec3(-0.1, 0.05, 0.0);
+        };
+        BlockPos treasureSpawnPos = pos.relative(facing);
+        int x = treasureSpawnPos.getX();
+        int y = treasureSpawnPos.getY();
+        int z = treasureSpawnPos.getZ();
+        ItemEntity squav = new ItemEntity(level, x + 0.5D, y + 0.5D, z + 0.5D, ModItems.SQUAVIGATOR.get().getDefaultInstance());
+        // North is negative Z I think
+        squav.setDeltaMovement(treasureVelocity);
+        level.addFreshEntity(squav);
+        ItemEntity squel = new ItemEntity(level, x + 0.5D, y + 0.5D, z + 0.5D, ModItems.SQUAVIGATOR.get().getDefaultInstance());
+        squel.setDeltaMovement(treasureVelocity);
+        level.addFreshEntity(squel);
     }
 }
