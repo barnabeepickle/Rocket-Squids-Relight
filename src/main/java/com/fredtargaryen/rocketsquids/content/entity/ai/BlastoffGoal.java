8c47574fd4ba155b97bc6c33b18b96f707293f61
fredtargaryen <lggy@hotmail.co.uk>
2026-04-01 23:08:41 +0100

    Simplify BlastoffGoal

--- src/main/java/com/fredtargaryen/rocketsquids/content/entity/ai/BlastoffGoal.java
+++ src/main/java/com/fredtargaryen/rocketsquids/content/entity/ai/BlastoffGoal.java
@@ -5,67 +5,51 @@ package com.fredtargaryen.rocketsquids.content.entity.ai;
 import com.fredtargaryen.rocketsquids.ModSounds;
 import com.fredtargaryen.rocketsquids.content.entity.RocketSquidEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.core.Direction;
-import net.minecraft.world.phys.Vec3;
 
-import java.util.ArrayList;
 import java.util.EnumSet;
 
 public class BlastoffGoal extends Goal {
+    /**
+     * Roughly how long blasts last, in AI ticks (of which there are only 10 per second)
+     */
+    private static final int blastTickLength = 15;
     private final RocketSquidEntity squid;
-    private boolean blastStarted;
-    private boolean horizontal;
+    private int blastTimer;
 
-    public BlastoffGoal(RocketSquidEntity ers)
-    {
+    public BlastoffGoal(RocketSquidEntity ers) {
         super();
         this.squid = ers;
         this.setFlags(EnumSet.of(Flag.MOVE));
-        this.blastStarted = false;
-        this.horizontal = true;
+        this.blastTimer = -1;
     }
 
     @Override
-    public boolean canUse()
-    {
+    public boolean canUse() {
         return this.squid.getBlasting() || this.squid.getForcedBlast();
     }
 
     @Override
     public void tick() {
-        Vec3 motion = this.squid.getDeltaMovement();
-        if (this.blastStarted) {
-            //The squid is part of the way through a blast
-            if((this.horizontal
-                    && this.motionHasPeaked(motion.x)
-                    && this.motionHasPeaked(motion.z))
-                    || (!this.horizontal && this.motionHasPeaked(motion.y))) {
-                //Squid has blasted but slowed down, i.e. end of blast
+        if (this.blastTimer < 0) {
+            // Begin blast
+            this.blastTimer = blastTickLength;
+            this.squid.playSound(ModSounds.BLASTOFF.get(), 1.0F, 1.0F);
+            this.squid.addForce(2.952);
+        } else if (this.blastTimer > 0) {
+            // Continue blast
+            this.blastTimer--;
+            this.squid.pointToWhereMoving();
+        } else {
+            if (this.squid.getForcedBlast()) {
+                // Explode, so resetting doesn't matter
+                this.squid.explode();
+            } else {
+                // Reset
                 this.squid.setShaking(false);
                 this.squid.setBlasting(false);
                 this.squid.hasImpulse = false;
-                this.blastStarted = false;
-                if(this.squid.getForcedBlast())
-                {
-                    this.squid.explode();
-                }
-            }
-            else {
-                this.squid.pointToWhereMoving();
+                this.blastTimer = -1;
             }
         }
-        else {
-            //Blast has not started yet
-            this.squid.setShaking(false);
-            this.squid.playSound(ModSounds.BLASTOFF.get(), 1.0F, 1.0F);
-            this.squid.addForce(2.952);
-            ArrayList<Direction> directionsPointing = this.squid.getDirectionsPointing();
-            this.horizontal = !directionsPointing.contains(Direction.UP) && !directionsPointing.contains(Direction.DOWN);
-            this.blastStarted = true;
-        }
-    }
-
-    private boolean motionHasPeaked(double motion) {
-        return Math.abs(motion) < 0.05;
     }
 }
